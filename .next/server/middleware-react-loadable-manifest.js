5a264a356ecaf32bb460d5af59819603aaae6b06
Salahuddin akhtar <Sakleonel51@gmail.com>
2025-09-12 17:22:34 +0530

    First Production without any next folder

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\page.tsx -> @/components/TextType\":{\"id\":\"app\\\\page.tsx -> @/components/TextType\",\"files\":[\"static/chunks/_app-pages-browser_components_TextType_tsx.js\"]}}"
